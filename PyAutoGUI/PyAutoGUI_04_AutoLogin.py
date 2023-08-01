4fdb9cb3cf9bee44d3e40bac7312cee72e123ee1
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-08-02 00:29:16 +0900

    study: Auto login

--- PyAutoGUI/PyAutoGUI_04_AutoLogin.py
+++ PyAutoGUI/PyAutoGUI_04_AutoLogin.py
@@ -1,24 +1,30 @@
+# 실습용 웹 사이트: http://dowellcomputer.com/
+# 아이디: pyautogui
+# 비밀번호: pyautogui
+
 import time
 import pyautogui
 
+time.sleep(3) # 3초간 기다리기(실습 환경 준비 시간)
+
 # 로그인 페이지로 이동
-page_location = pyautogui.locateOnScreen("login_1.png", confidence=0.5)
+page_location = pyautogui.locateOnScreen("Image\login_1.png", confidence=0.5)
 pyautogui.moveTo(page_location)
 pyautogui.click()
 
 time.sleep(1) # 1초간 기다리기
 
 # 아이디 입력
-id_location = pyautogui.locateOnScreen("id.png", confidence=0.6)
+id_location = pyautogui.locateOnScreen("Image\id.png", confidence=0.6)
 pyautogui.moveTo(id_location) # 커서를 아이디 위치로 이동
-pyautogui.moveRel(300, 0) # 오른쪽으로 300px 이동
+#pyautogui.moveRel(300, 0) # 오른쪽으로 300px 이동
 pyautogui.click() # 입력창 클릭
 pyautogui.typewrite("pyautogui") # 아이디 입력
 
 # 비밀번호 입력
-password_location = pyautogui.locateOnScreen("password.png", confidence=0.6)
+password_location = pyautogui.locateOnScreen("Image\password.png", confidence=0.6)
 pyautogui.moveTo(password_location) # 커서를 비밀번호 위치로 이동
-pyautogui.moveRel(300, 0) # 오른쪽으로 300px 이동
+#pyautogui.moveRel(300, 0) # 오른쪽으로 300px 이동
 pyautogui.click() # 입력창 클릭
 pyautogui.typewrite("pyautogui") # 비밀번호 입력
 
